591939468297101051587f334f3a506612d34021
LLFourn <lloyd.fourn@gmail.com>
2021-05-14 16:01:21 +1000

    Get our own data from mempool.space

--- app.py
+++ app.py
@@ -172,8 +172,40 @@ def inconsistent_ma(df):
     fig.update_yaxes(categoryorder='max ascending', title=None)
     return fig
 
+def check_next_block_mempoolio(df, miner_match):
+    if df is None or len(df) < 1:
+        height = 681418
+        df = pd.DataFrame(columns=['height', 'miner', 'signal'])
+    else:
+        height = df.iat[-1,0] + 1;
+    r = rq.get("https://mempool.space/api/block-height/" + str(height))
+    if r.status_code == 200:
+        block_id = r.text
+        block_info = rq.get("https://mempool.space/api/block/" + block_id).json()
+        signal = (block_info["version"] & 0x04) == 0x04
+        coinbase_txid = rq.get("https://mempool.space/api/block/" + block_id + "/txid/0").text
+        res = rq.get("https://mempool.space/api/tx/" + coinbase_txid)
+        res.raise_for_status()
+        coinbase_tx = res.json()
+        coinbase_address = next(vout['scriptpubkey_address'] for vout in  coinbase_tx['vout'] if vout['scriptpubkey_type'] != 'op_return')
+        coinbase_tag = bytes.fromhex(coinbase_tx['vin'][0]['scriptsig']).decode('utf-8', 'replace')
+        miner = next((info['name'] for (tag,info) in miner_match['coinbase_tags'].items() if tag in coinbase_tag), None)
+        if miner is None:
+            miner = miner_match['payout_addresses'][coinbase_address]['name'] or "unknown"
+        new_row = pd.DataFrame(data= { 'height': [height], 'signal' : [signal], 'miner' : [miner] })
+        print(height, miner, signal)
+        df = df.append(new_row)
+        df.to_csv("data.csv", index=False)
+    elif r.status_code == 404:
+        time.sleep(60)
+    else:
+        print(r.status_code, r.text)
+        time.sleep(600)
 
-def steal_data():
+    return df
+
+# can't get all data from here now https://github.com/hsjoberg/fork-explorer/issues/58
+def steal_data_taproot_watch():
     r = rq.get("https://taproot.watch/blocks")
     if r.status_code == 200:
         json = r.json()
@@ -183,6 +215,5 @@ def steal_data():
         r.raise_for_status()
 
 if __name__ == '__main__':
-    # To actually run in production use waitress_server.py
-#    steal_data()
+#    To actually run in production use waitress_server.py
     app.run_server(debug=True)
